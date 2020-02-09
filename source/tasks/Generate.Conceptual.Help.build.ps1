159df04a658dcbfa6e653ea11f0c26a815bef0bc
Johan Ljunggren <johan.ljunggren@shamn.se>
2020-02-09 17:19:01 +0100

    Minor style changes

--- source/tasks/Generate.Conceptual.Help.build.ps1
+++ source/tasks/Generate.Conceptual.Help.build.ps1
@@ -39,7 +39,7 @@ param
 
     [Parameter()]
     [string]
-    $SourcePath = (property SourcePath ((Get-ChildItem $BuildRoot\*\*.psd1 -Exclude 'build.psd1', 'analyzersettings.psd1' | Where-Object {
+    $SourcePath = (property SourcePath ((Get-ChildItem $BuildRoot\*\*.psd1 -Exclude @('build.psd1', 'analyzersettings.psd1') | Where-Object {
                     ($_.Directory.Name -match 'source|src' -or $_.Directory.Name -eq $_.BaseName) -and
                     $(try
                         {
