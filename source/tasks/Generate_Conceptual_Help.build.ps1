8c5bfe4ffdd013ca75b61e7367445f58ec289085
Johan Ljunggren <johan.ljunggren@shamn.se>
2020-02-10 18:35:50 +0100

    Update comment-based help

--- source/tasks/Generate_Conceptual_Help.build.ps1
+++ source/tasks/Generate_Conceptual_Help.build.ps1
@@ -2,59 +2,87 @@
     .SYNOPSIS
         This is a build task that generates conceptual help.
 
+    .PARAMETER ProjectPath
+        The root path to the project. Defaults to $BuildRoot.
+
+    .PARAMETER OutputDirectory
+        The base directory of all output. Defaults to folder 'output' relative to
+        the $BuildRoot.
+
+    .PARAMETER ProjectName
+        The project name. Defaults to the BaseName of the module manifest it finds
+        in either the folder 'source', 'src, or a folder with the same name as
+        the module.
+
+    .PARAMETER SourcePath
+        The path to the source folder name. Defaults to the same path where the
+        module manifest is found.
+
+    .PARAMETER ModuleVersion
+        The module version of the build module. Defaults to the semantic version
+        returned by GitVersion. If GitVersion is not present, the default is
+        version '0.0.1'.
+
+    .PARAMETER BuildInfo
+        The build info object from ModuleBuilder. Defaults to an empty hashtable.
+
     .NOTES
         This is a build task is primarily meant to be run by Invoke-Build but
         wrapped by the Sampler project's build.ps1 (https://github.com/gaelcolas/Sampler).
 #>
 param
 (
-    # Project path
     [Parameter()]
-    [string]
+    [System.String]
     $ProjectPath = (property ProjectPath $BuildRoot),
 
     [Parameter()]
-    # Base directory of all output (default to 'output')
-    [string]
+    [System.String]
     $OutputDirectory = (property OutputDirectory (Join-Path $BuildRoot 'output')),
 
     [Parameter()]
-    [string]
+    [System.String]
     $ProjectName = (property ProjectName $(
-            #Find the module manifest to deduce the Project Name
-            (Get-ChildItem $BuildRoot\*\*.psd1 -Exclude 'build.psd1', 'analyzersettings.psd1' | Where-Object {
+            # Find the module manifest to deduce the Project Name
+            (Get-ChildItem $BuildRoot\*\*.psd1 -Exclude @('build.psd1', 'analyzersettings.psd1') | Where-Object -FilterScript {
                     ($_.Directory.Name -match 'source|src' -or $_.Directory.Name -eq $_.BaseName) -and
-                    $(try
+                    $(
+                        try
                         {
                             Test-ModuleManifest $_.FullName -ErrorAction Stop
                         }
                         catch
                         {
-                            Write-Warning $_
+                            Write-Warning -Message $_
                             $false
-                        }) }
-            ).BaseName
+                        }
+                    )
+                }).BaseName
         )
     ),
 
     [Parameter()]
-    [string]
-    $SourcePath = (property SourcePath ((Get-ChildItem $BuildRoot\*\*.psd1 -Exclude @('build.psd1', 'analyzersettings.psd1') | Where-Object {
+    [System.String]
+    $SourcePath = (property SourcePath $(
+            (Get-ChildItem $BuildRoot\*\*.psd1 -Exclude @('build.psd1', 'analyzersettings.psd1') | Where-Object -FilterScript {
                     ($_.Directory.Name -match 'source|src' -or $_.Directory.Name -eq $_.BaseName) -and
-                    $(try
+                    $(
+                        try
                         {
                             Test-ModuleManifest $_.FullName -ErrorAction Stop
                         }
                         catch
                         {
-                            Write-Warning $_
+                            Write-Warning -Message $_
                             $false
-                        }) }
-            ).Directory.FullName)
+                        }
+                    )
+                }).Directory.FullName
+        )
     ),
 
     [Parameter()]
-    [string]
+    [System.String]
     $ModuleVersion = (property ModuleVersion $(
             try
             {
@@ -67,7 +95,6 @@ param
             }
         )),
 
-    # Build Configuration object
     [Parameter()]
     $BuildInfo = (property BuildInfo @{ })
 )
