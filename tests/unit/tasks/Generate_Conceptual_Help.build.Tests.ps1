b66869cb9ffb785cbbf175ef0bf24fa1e758da45
Johan Ljunggren <johan.ljunggren@shamn.se>
2020-07-06 12:43:11 +0200

    Fix unit tests

--- tests/unit/tasks/Generate_Conceptual_Help.build.Tests.ps1
+++ tests/unit/tasks/Generate_Conceptual_Help.build.Tests.ps1
@@ -74,4 +74,35 @@ Describe 'Generate_Conceptual_Help' {
             } -Exactly -Times 1 -Scope It
         }
     }
+
+    Context 'When the generating conceptual help and parsing markdown code for a built module' {
+        BeforeAll {
+            # This mocks the alias 'property' for the parameter 'ModuleVersion'.
+            Mock -Command Get-BuiltModuleVersion -MockWith {
+                return '99.1.1-preview0001'
+            }
+
+            $mockTaskParameters = @{
+                ProjectName = 'MyModule'
+                SourcePath = $TestDrive
+                MarkdownCodeRegularExpression = @(
+                    '\`(.+?)\`'
+                )
+            }
+
+            $mockExpectedDestinationModulePath = Join-Path -Path $script:projectPath -ChildPath 'output' |
+                Join-Path -ChildPath $mockTaskParameters.ProjectName |
+                    Join-Path -ChildPath '99.1.1'
+        }
+
+        It 'Should run the build task with the correct destination module path and without throwing' {
+            {
+                Invoke-Build -Task $buildTaskName -File $script:buildScript.Definition @mockTaskParameters
+            } | Should -Not -Throw
+
+            Assert-MockCalled -CommandName New-DscResourcePowerShellHelp -ParameterFilter {
+                $DestinationModulePath -eq $mockExpectedDestinationModulePath
+            } -Exactly -Times 1 -Scope It
+        }
+    }
 }
