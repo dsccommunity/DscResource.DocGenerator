6d0cadbd7d2dddf0628c5e61acd25e41ee6c90fe
Daniel Hughes <daniel.hughes@hetalogic.com>
2024-11-06 15:00:16 +0000

    Update help

--- source/Private/Test-ClassPropertyDscAttributeArgument.ps1
+++ source/Private/Test-ClassPropertyDscAttributeArgument.ps1
@@ -7,9 +7,9 @@
         This function returns the property state value of an DSC class-based
         resource property.
 
-    .PARAMETER Ast
-        The Abstract Syntax Tree (AST) for class-based DSC resource property.
-        The passed value must be an AST of the type 'PropertyMemberAst'.
+    .PARAMETER PropertyAttributes
+        The PropertyAttributes for class-based DSC resource property.
+        The passed value must be of the type 'System.Reflection.CustomAttributeData'.
 
     .PARAMETER IsKey
         Specifies if the parameter is expected to have the type qualifier Key.
@@ -24,29 +24,10 @@
         Specifies if the parameter is expected to have the type qualifier Read.
 
     .EXAMPLE
-        Test-PropertyMemberAst -IsKey -Ast {
-            [DscResource()]
-            class NameOfResource {
-                [DscProperty(Key)]
-                [string] $KeyName
-
-                [NameOfResource] Get() {
-                    return $this
-                }
-
-                [void] Set() {}
-
-                [bool] Test() {
-                    return $true
-                }
-            }
-        }.Ast.Find({$args[0] -is [System.Management.Automation.Language.PropertyMemberAst]}, $false)
+        Test-ClassPropertyDscAttributeArgument -IsKey -PropertyAttributes $properties
 
         Returns $true that the property 'KeyName' is Key.
 #>
-
-# From provided CustomAttribute
-# Return desired Dsc value if true e.g. Key|Mandatory|Read|Write
 
 function Test-ClassPropertyDscAttributeArgument
 {
