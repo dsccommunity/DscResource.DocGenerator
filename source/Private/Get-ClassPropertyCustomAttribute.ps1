61287442dfddc5b375d882d850a7c4384b4f42db
Daniel Hughes <daniel.hughes@hetalogic.com>
2024-11-06 17:30:41 +0000

    Add comment based help

--- source/Private/Get-ClassPropertyCustomAttribute.ps1
+++ source/Private/Get-ClassPropertyCustomAttribute.ps1
@@ -1,6 +1,27 @@
+<#
+    .SYNOPSIS
+        This function returns a filtered list of CustomAttributes from a given AttributeType.
+
+    .DESCRIPTION
+        The function will filter the provided CustomAttributes provided and returns only the matching attributes or null.
+
+    .PARAMETER Attributes
+        The array of attributes that need to be filtered.
+
+    .PARAMETER AttributeType
+        The attribute type name to filter the attributes on.
+
+    .OUTPUTS
+        System.Reflection.CustomAttributeData[]
+
+    .EXAMPLE
+        Get-ClassPropertyCustomAttribute -Attributes $CommonAttributes -AttributeType 'ValidateSetAttribute'
+#>
+
 function Get-ClassPropertyCustomAttribute
 {
     [CmdletBinding()]
+    [OutputType([System.Reflection.CustomAttributeData[]])]
     param (
         [Parameter(Mandatory = $true)]
         [System.Reflection.CustomAttributeData[]]
